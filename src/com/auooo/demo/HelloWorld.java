b50f07ab686e588b85f86115046fdb2cc486cb40
auooo <mh245500@gmail.com>
2015-11-29 22:27:41 +0800

    添加测试md文件

---
+++ src/com/auooo/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.auooo.demo;
+
+/**
+ * Created by Hong on 2015/11/29.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld123");
+    }
+}
